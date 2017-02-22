14a1c1f1165eb4e2cb8a91f4a03e6641115aabc2
Brian Faust <hello@brianfaust.de>
2017-02-22 08:16:19 +0200

    Add Patreon note

--- src/TaxCalculatorServiceProvider.php
+++ src/TaxCalculatorServiceProvider.php
@@ -11,15 +11,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Tax Calculator.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace BrianFaust\TaxCalculator;
 
 use BrianFaust\ServiceProvider\AbstractServiceProvider;
