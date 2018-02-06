82a420a890ed7c88b3c08c96e72ae43e140894a1
Brian Faust <faustbrian@users.noreply.github.com>
2018-02-06 14:38:13 +0200

    Update ServiceProviderTest.php

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace BrianFaust\Tests\TaxCalculator;
 
+use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
+
 class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTrait;
