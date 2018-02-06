1b81159e76ebaf0356d7dd32357ea9a96d14c62c
Brian Faust <faustbrian@users.noreply.github.com>
2018-02-06 14:03:22 +0200

    Apply fixes from StyleCI (#9)
    [ci skip] [skip ci]

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -13,8 +13,6 @@ declare(strict_types=1);
 
 namespace BrianFaust\Tests\TaxCalculator;
 
-use BrianFaust\TaxCalculator\TaxCalculatorServiceProvider;
-
 class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTrait;
