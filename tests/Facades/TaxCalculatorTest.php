515a77581c7b0cc0c89dfaad8c55469b4bac6640
Brian Faust <faustbrian@users.noreply.github.com>
2018-02-12 09:12:49 +0200

    Apply fixes from StyleCI (#11)
    [ci skip] [skip ci]

--- tests/Facades/TaxCalculatorTest.php
+++ tests/Facades/TaxCalculatorTest.php
@@ -14,9 +14,9 @@ declare(strict_types=1);
 namespace BrianFaust\Tests\TaxCalculator\tests\Facades;
 
 use BrianFaust\TaxCalculator\Calculator;
-use GrahamCampbell\TestBenchCore\FacadeTrait;
 use BrianFaust\TaxCalculator\Facades\TaxCalculator;
 use BrianFaust\Tests\TaxCalculator\AbstractTestCase;
+use GrahamCampbell\TestBenchCore\FacadeTrait;
 
 class TaxCalculatorTest extends AbstractTestCase
 {
